6514769695525bd39bb26792e8fe3224a1c594e8
Rob Cermak <rob.cermak@gmail.com>
2024-04-01 23:38:31 -0700

    Update HashDigestTests.java hashes
     * Fix test for issue#140
     * Show file being hashed
     * Update md5 hash
     * Update sha256 hash

--- src/test/java/com/cohort/util/HashDigestTests.java
+++ src/test/java/com/cohort/util/HashDigestTests.java
@@ -8,6 +8,7 @@ class HashDigestTests {
   void basicTest() throws Throwable {
     System.out.println("*** HashDigest.basicTest");
     String tName = HashDigestTests.class.getResource("LICENSE.txt").getPath();
+    System.out.println("*** Checksum test file: " + tName);
     Test.ensureEqual(HashDigest.doIt(new String[] { "type:MD5" }),
         "Neither password or filename was specified.\n" + HashDigest.usage, "");
     Test.ensureEqual(HashDigest.doIt(new String[] { "password:myPassword", "type:MD-5" }),
@@ -19,13 +20,13 @@ class HashDigestTests {
     Test.ensureEqual(HashDigest.doIt(new String[] { "password:myPassword", "type:SHA-256" }),
         "76549b827ec46e705fd03831813fa52172338f0dfcbd711ed44b81a96dac51c6", "");
     Test.ensureEqual(HashDigest.doIt(new String[] { "filename:" + tName, "type:MD5" }),
-        "96e9b4d974b734874a66f433b276a41a", "");
+        "327fbb2aa6c6297d4fdd5fdf4b14e289", "");
     Test.ensureEqual(HashDigest.doIt(new String[] { "filename:" + tName, "type:SHA-256" }),
-        "f6afea3f5fa69c8e54b14b3fdb86a9aa261a423cea957c9c87b55b92441d56bb", "");
+        "e376c88953b2d56b00783fed071f1875e8ed94230f4e14eee5bce8bd608de5e6", "");
     Test.ensureEqual(HashDigest.doIt(new String[] { "filename:" + tName, "type:SHA-256", "-file" }),
         "Created " + tName + ".sha256", "");
     Test.ensureEqual(File2.readFromFileUtf8(tName + ".sha256")[1],
-        "f6afea3f5fa69c8e54b14b3fdb86a9aa261a423cea957c9c87b55b92441d56bb  LICENSE.txt\n", "");
+        "e376c88953b2d56b00783fed071f1875e8ed94230f4e14eee5bce8bd608de5e6  LICENSE.txt\n", "");
     File2.delete(tName + ".sha256");
   }
 }
